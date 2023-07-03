664f592a39e33f536b79e001bb2dbe1e3514048a
ReeseTodhunter <reeset538@gmail.com>
2023-07-03 23:09:38 +0100

    Update
    Icons added to the Navigation bar
    The main "About Me" section work began
    Animated scroll added when clicking on an option on the Nav bar.

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+// Smooth scrolling
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('nav a');
+  
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var targetSection = document.querySelector(this.getAttribute('href'));
+        scrollToSection(targetSection.offsetTop);
+      });
+    }
+  
+    function scrollToSection(offset) {
+      var scrollOptions = {
+        top: offset,
+        behavior: 'smooth'
+      };
+      window.scrollTo(scrollOptions);
+    }
+});
