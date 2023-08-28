fe00d74824c8dac220db4a56799469dc5de8278f
ReeseTodhunter <reeset538@gmail.com>
2023-08-28 01:29:27 +0100

    Smooth scrolling and link fixes for various pages

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
-
-//Transparent navbar
+// Transparent navbar
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
   if (window.scrollY > 50) {
-      navbar.classList.add('transparent');
+    navbar.classList.add('transparent');
   } else {
-      navbar.classList.remove('transparent');
+    navbar.classList.remove('transparent');
   }
 });
 
 // Smooth scrolling
 document.addEventListener('DOMContentLoaded', function() {
-    var navLinks = document.querySelectorAll('nav a');
-  
-    for (var i = 0; i < navLinks.length; i++) {
-      navLinks[i].addEventListener('click', function(e) {
+  var navLinks = document.querySelectorAll('nav a');
+  var logoLink = document.getElementById('navbar__logo');
+  var currentPage = window.location.pathname;
+
+  if (logoLink && (currentPage === '/index.html' || currentPage === '/')) {
+    logoLink.addEventListener('click', function(event) {
+      const target = document.querySelector(this.getAttribute('href'));
+
+      if (target) {
+        event.preventDefault();
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  }
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      const target = document.querySelector(this.getAttribute('href'));
+
+      if (target) {
         e.preventDefault();
-        var targetSection = document.querySelector(this.getAttribute('href'));
-        scrollToSection(targetSection.offsetTop);
-      });
-    }
-  
-    function scrollToSection(offset) {
-      var scrollOptions = {
-        top: offset,
-        behavior: 'smooth'
-      };
-      window.scrollTo(scrollOptions);
-    }
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
 });
 
-//Return to the top of the page or to the home page
-document.addEventListener('DOMContentLoaded', function() {
-  const logoLink = document.getElementById('navbar__logo');
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-  logoLink.addEventListener('click', function(event) {
-      const currentPage = window.location.pathname;
-
-      if (currentPage === '/index.html' || currentPage === '/') {
-          // If on index page, scroll to the top
-          event.preventDefault();
-          window.scrollTo({
-              top: 0,
-              behavior: 'smooth'
-          });
-      }
-      // Otherwise, let the link behave as normal
-  });
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 });
